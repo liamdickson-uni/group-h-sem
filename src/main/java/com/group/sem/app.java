4b6d414a0580a78c3cb497192fb4b6d2af704efe
Liam Dickson <40456372@live.napier.ac.uk>
2021-02-08 18:04:28 +0000

    Fixed Bugs

--- src/main/java/com/group/sem/app.java
+++ src/main/java/com/group/sem/app.java
@@ -5,13 +5,12 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
-public class app {
-
-    //Docker Test Code
-    public static void main(String[] args) {
-
+public class app
+{
+    public static void main(String[] args)
+    {
         // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("mongo-dbserver");
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
@@ -27,6 +26,5 @@ public class app {
         // Check document in collection
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
-
     }
 }
