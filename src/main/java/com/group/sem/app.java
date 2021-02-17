93f3128e6218bbb9d89de5f0a8e317ac63ec6560
Tom McEachan <tom.a.mceachan@outlook.com>
2021-02-17 19:57:39 +0000

    Edited app.java and country.java to query world.sql database.

--- src/main/java/com/group/sem/app.java
+++ src/main/java/com/group/sem/app.java
@@ -2,18 +2,26 @@ package com.group.sem;
 
 import java.sql.*;
 
-public class app{
+import com.group.sem.country;
 
-    public static void main(String[] args)
-    {
+public class app {
+
+    public static void main(String[] args) {
         // Create new Application
         app a = new app();
 
         // Connect to database
         a.connect();
 
+        //Gets country
+        country cnt = a.getCountry(1);
+
+        //Displays country
+        a.displayCountry(cnt);
+
         // Disconnect from database
         a.disconnect();
+
     }
 
     /**
@@ -24,39 +32,29 @@ public class app{
     /**
      * Connect to the MySQL database.
      */
-    public void connect()
-    {
-        try
-        {
+    public void connect() {
+        try {
             // Load Database driver
             Class.forName("com.mysql.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
         int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "password");
                 System.out.println("Successfully connected");
                 break;
-            }
-            catch (SQLException sqle)
-            {
+            } catch (SQLException sqle) {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
@@ -65,19 +63,46 @@ public class app{
     /**
      * Disconnects from the MySQL database.
      */
-    public void disconnect()
-    {
-        if (con != null)
-        {
-            try
-            {
+    public void disconnect() {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
+        }
+    }
+
+    public country getCountry(int ID) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Code, Name"
+                            + "FROM country ";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next()) {
+                country cnt = new country();
+                cnt.ID = rset.getInt("emp_no");
+                return cnt;
+            } else
+                return null;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+
+    public void displayCountry(country cnt) {
+        if (cnt != null) {
+            System.out.println(
+                    cnt.ID + " " + cnt.countryName);
         }
     }
 }
---
+++ src/main/java/com/group/sem/country.java
@@ -0,0 +1,36 @@
+package com.group.sem;
+
+import java.sql.*;
+import com.group.sem.app;
+
+/**
+ * Represents a country
+ */
+public class country {
+
+    /**
+     * Represents Country ID
+     */
+    public int ID;
+
+    /**
+     * Represents Country Name
+     */
+    public String countryName;
+
+    /**
+     * Represents Country Code
+     */
+    public String countryCode;
+
+    /**
+     * Represents District
+     */
+    public String countryDistrict;
+
+    /** Represents Population
+     *
+     */
+    public int countryPopulation;
+
+}
