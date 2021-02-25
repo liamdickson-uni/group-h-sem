45d9bbd511f524f9639191db3ff2d49fa9eca8b8
Tom McEachan <tom.a.mceachan@outlook.com>
2021-02-25 19:01:50 +0000

    Created city.java and the necessary classes. Added schema.png again as it seems to have been lost at some point.

---
+++ src/main/java/com/group/sem/city.java
@@ -0,0 +1,38 @@
+package com.group.sem;
+
+
+/**
+ * This class represents a city
+ */
+
+public class city {
+
+
+    /**
+     * Represents a City
+     */
+    public String cityID;
+
+    /**
+     * Represents a City Name
+     */
+    public String cityName;
+
+
+    /**
+     * Represents a Country Code
+     */
+    public String countryCode;
+
+
+    /**
+     * Represents a City District
+     */
+    public String cityDistrict;
+
+    /**
+     * Represents a City Population
+     */
+    public String cityPopulation;
+
+}
