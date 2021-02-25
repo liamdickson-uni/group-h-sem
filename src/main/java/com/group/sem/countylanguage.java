d665733b7a01173877526df3ae677a35df72c1b0
Greig Dunbar <40430731@live.napier.ac.uk>
2021-02-25 18:05:30 +0000

    Addition of countylanguage.java

---
+++ src/main/java/com/group/sem/countylanguage.java
@@ -0,0 +1,15 @@
+package com.group.sem;
+
+import java.sql.*;
+import com.group.sem.app;
+
+public class countylanguage {
+
+    public String CountryCode;
+
+    public String Lanuage;
+
+    public Enum IsOfficial;
+
+    public double Percentage;
+}
