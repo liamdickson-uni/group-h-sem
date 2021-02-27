fdf571abec483955a9ca64f78c6bba5838a5c39d
Liam Dickson <40456372@live.napier.ac.uk>
2021-02-27 15:05:55 +0000

    Moved Get methods in to the classes they get from and created queries for the user issues: "As a Regional Finance Manager I would like to see a list of all the countries in a region organised by largest population to smallest so that I can identify the most to least populous countries in that region. This would allow me to decide which nations we are likely to get the most profit from and which the least." and "As a global marketing manager I would like to see a list of all the cities in the world organised by largest population to smallest so that I can identify the most populous cities to the smallest cities in one list for use in an upcoming product launch."

---
+++ src/main/java/com/group/sem/App.java
@@ -0,0 +1,196 @@
+package com.group.sem;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class App {
+
+    public static void main(String[] args) {
+        // Create new Application
+        App a = new App();
+        Country c = new Country();
+        City cc = new City();
+
+        // Connect to database
+        a.connect();
+
+        System.out.println("Please Select an Option:\n " +
+                "1 - Get all counties by population\n " +
+                "2 - Get all countries in a specific continent\n" +
+                "3 - Get all countries in a specific region\n" +
+                "4 - Get all cities in a specific country\n" +
+                "5 - Get all cities ordered by population\n");
+
+        String userInput = "3";
+
+        if (userInput.equals("1")) {
+            //Gets all countries ordered by population largest to smallest
+            ArrayList<Country> countries = c.getCountryByPopDesc();
+
+            //Displays list of selected query
+            a.displayCountry(countries, userInput);
+        } else if (userInput.equals("2")) {
+            //Gets all countries by a selected continent ordered by population largest to smallest
+            ArrayList<Country> countries = c.getCountryInContinentByPop();
+
+            //Displays list of selected query
+            a.displayCountry(countries, userInput);
+
+        } else if (userInput.equals("3")) {
+            //Gets all countries in a selected region ordered by population largest to smallest
+            ArrayList<Country> countries = c.getCountryInRegionByPop();
+
+            //Displays list of selected query
+            a.displayCountry(countries, userInput);
+
+        } else if (userInput.equals("4")) {
+            //Gets all cities in a selected country ordered by population largest to smallest
+            ArrayList<City> cities = cc.getCitiesInCountryByPop();
+
+            //Displays list of selected query
+            a.displayCity(cities, userInput);
+
+        } else if (userInput.equals("5")) {
+            //Gets all cities ordered by population largest to smallest
+            ArrayList<City> cities = cc.getCitiesByPop();
+
+            //Displays list of selected query
+            a.displayCity(cities, userInput);
+
+        }
+
+        // Disconnect from database
+        a.disconnect();
+
+    }
+
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect() {
+        try {
+            // Load Database driver
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i) {
+            System.out.println("Connecting to database...");
+            try {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            } catch (SQLException sqle) {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println(sqle.getMessage());
+            } catch (InterruptedException ie) {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+
+    /**
+     * Disconnects from the MySQL database.
+     */
+    public void disconnect() {
+        if (con != null) {
+            try {
+                // Close connection
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+
+
+    public void displayCountry(ArrayList<Country> countries, String userInput) {
+
+        if (userInput.equals("1")) {
+            if (countries != null) {
+
+                //Prints Column Header
+                System.out.printf("%-45s %-15s", "Country", "Population\n");
+
+                //Loops over all the countries in the database
+                for (Country country : countries) {
+                    String output = String.format("%-45s %-15s", country.Name, country.Population);
+                    System.out.println(output);
+                }
+            }
+        } else if (userInput.equals("2")) {
+
+            //Prints Column Header
+            System.out.printf("%-20s %-15s", "Continent", "Country\n");
+
+            if (countries != null) {
+                for (Country country : countries) {
+                    String output = String.format("%-20s %-15s", country.Continent, country.Name);
+                    System.out.println(output);
+
+                }
+            }
+        } else if (userInput.equals("3")) {
+
+            //Prints Column Header
+            System.out.printf("%-20s %-15s %-15s", "Region", "Country", "Population\n");
+
+            if (countries != null) {
+                for (Country country : countries) {
+                    String output = String.format("%-20s %-15s %-15s", country.Region, country.Name, country.Population);
+                    System.out.println(output);
+
+                }
+            }
+        }
+
+    }
+
+
+    public void displayCity(ArrayList<City> cities, String userInput) {
+
+        if (userInput.equals("3")) {
+
+            //Prints Column Header
+            System.out.printf("%-20s", "City\n");
+
+            if (cities != null) {
+
+                for (City city : cities) {
+                    System.out.println(city.cityName);
+                }
+            }
+
+        } else if (userInput.equals("4")) {
+
+            //Prints Column Header
+            System.out.printf("%-20s %-15s", "City", "Population\n");
+
+            if (cities != null) {
+
+                for (City city : cities) {
+                    String output = String.format("%-45s %-15s", city.cityName, city.cityPopulation);
+                    System.out.println(output);
+                }
+            }
+
+        }
+
+
+    }
+
+}
+
+
+
---
+++ src/main/java/com/group/sem/City.java
@@ -0,0 +1,115 @@
+package com.group.sem;
+
+import com.mysql.fabric.jdbc.FabricMySQLConnectionProxy;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/*
+ * This class represents a city
+ */
+
+public class City {
+
+
+    /*
+     * Represents a City
+     */
+    public String cityID;
+
+    /*
+     * Represents a City Name
+     */
+    public String cityName;
+
+
+    /*
+     * Represents a Country Code
+     */
+    public String countryCode;
+
+
+    /*
+     * Represents a City District
+     */
+    public String cityDistrict;
+
+    /*
+     * Represents a City Population
+     */
+    public int cityPopulation;
+
+
+    /*
+     * These methods are used to get city data and to display city data.
+     */
+    private Connection con = null;
+
+    public ArrayList<City> getCitiesInCountryByPop() {
+
+        try {
+            // Create an SQL statement
+            con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    " SELECT cty.Name" +
+                            " FROM city cty" +
+                            " JOIN country cnt ON (cnt.Code = cty.CountryCode) " +
+                            "WHERE cnt.Name IN ('France')" +
+                            "ORDER BY cty.Population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new country while valid.
+
+            ArrayList<City> cities = new ArrayList<>();
+
+            // Check one is returned
+            while (rset.next()) {
+                City cty = new City();
+                cty.cityName = rset.getString("Name");
+                cities.add(cty);
+            }
+            return cities;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get cities in selected country");
+            return null;
+
+        }
+    }
+
+    public ArrayList<City> getCitiesByPop() {
+
+        try {
+            // Create an SQL statement
+            con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    " SELECT c.Name, c.Population FROM city c ORDER BY c.Population DESC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new country while valid.
+
+            ArrayList<City> cities = new ArrayList<>();
+
+            // Check one is returned
+            while (rset.next()) {
+                City cty = new City();
+                cty.cityName = rset.getString("Name");
+                cty.cityPopulation = rset.getInt("Population");
+                cities.add(cty);
+            }
+            return cities;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get cities by population");
+            return null;
+
+        }
+    }
+
+}
