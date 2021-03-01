b194f850b0f54fcd3d65a18bbf9efb94916ec982
Greig Dunbar <40430731@live.napier.ac.uk>
2021-03-01 19:35:48 +0000

    Working Cities in region feature

--- src/main/java/com/group/sem/App.java
+++ src/main/java/com/group/sem/App.java
@@ -19,9 +19,11 @@ public class App {
                 "2 - Get all countries in a specific continent\n" +
                 "3 - Get all countries in a specific region\n" +
                 "4 - Get all cities in a specific country\n" +
-                "5 - Get all cities ordered by population\n");
+                "5 - Get all cities ordered by population\n" +
+                "8 - Get all cities in a region"
+        );
 
-        String userInput = "3";
+        String userInput = "8";
 
         if (userInput.equals("1")) {
             //Gets all countries ordered by population largest to smallest
@@ -56,6 +58,15 @@ public class App {
 
             //Displays list of selected query
             a.displayCity(cities, userInput);
+
+        }else if (userInput.equals("8")){
+            //Get all cities in a region ordered by population largest to smallest
+            ArrayList<City> cities = cc.getCitiesInRegion();
+
+            //Displays list of selected query
+            a.displayCity(cities, userInput);
+
+
 
         }
 
@@ -185,6 +196,17 @@ public class App {
                 }
             }
 
+        }
+        else if (userInput.equals("8")){
+
+            System.out.printf("City\n");
+
+            if (cities != null) {
+
+                for (City city : cities) {
+                    System.out.println(city.cityName);
+                }
+            }
         }
 
 
