1872710f8e3a57d96bb747fd2fbf21c7396455b5
Tom McEachan <tom.a.mceachan@outlook.com>
2021-03-17 14:44:17 +0000

    Added dummy integration test and updated travis.yml to reflect this.

--- src/test/java/com/group/sem/AppIntegrationTest.java
+++ src/test/java/com/group/sem/AppIntegrationTest.java
@@ -1,4 +1,32 @@
 package com.group.sem;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AppIntegrationTest {
+
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("db:3306");
+
+
+    }
+
+    @Test
+    void testGetCountry()
+    {
+        Country c = new Country();
+        ArrayList<Country> countries = c.getCountryByPopDesc();
+        app.displayCountry(countries, "1");
+        assertEquals(c.Code, "1");
+    }
+
+
 }
