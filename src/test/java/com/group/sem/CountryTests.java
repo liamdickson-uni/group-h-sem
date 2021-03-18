33501bad831621ff1fc2eb875d602e697e75a3ac
Tom McEachan <tom.a.mceachan@outlook.com>
2021-03-18 20:23:48 +0000

    Began populating CountryTests.java

--- src/test/java/com/group/sem/CountryTests.java
+++ src/test/java/com/group/sem/CountryTests.java
@@ -1,18 +1,43 @@
 package com.group.sem;
 
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.rmi.server.ExportException;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+
 public class CountryTests {
 
     static Country country;
+    static App app;
 
     @BeforeAll
     static void init() {
         country = new Country();
+        app = new App();
     }
 
     @Test
-    void className()
+    void getCountryInContinentByPopCanConnect()
+    {
+        ArrayList<Country> countries = country.getCountryInContinentByPop();
+    }
+
+    @Test
+    void getCountryInContinentByPopIsNotNull()
+    {
+        ArrayList<Country> countries = country.getCountryInContinentByPop();
+        for (Country country: countries){
+        assertNotNull(countries);}
+    }
+
+
+    @Test
+    void getCountryInContinentByPopThrowsException()
     {
 
     }
