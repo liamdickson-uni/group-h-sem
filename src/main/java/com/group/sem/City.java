c190bef44a24d5df9666bd1dee60a3e86a194432
Liam Dickson <40456372@live.napier.ac.uk>
2021-04-24 14:33:50 +0100

    Reformatted code to remove duplication and added setNGetCitiesInContinentByPop() feature and tests

--- src/main/java/com/group/sem/City.java
+++ src/main/java/com/group/sem/City.java
@@ -866,4 +866,56 @@ public class City {
         }
 
     }
+
+    /**
+     * This method gets a specified list of cities in a continent ordered by population
+     *
+     * @return an ArrayList of Cities
+     */
+    public ArrayList<City> setNGetCitiesInContinentByPop(String userContinent, int limit) {
+
+        try {
+
+            //Defines the prepared SQL statement
+            String sql = " SELECT cty.Name" +
+                    " FROM city cty" +
+                    " INNER JOIN country c ON c.code = cty.CountryCode" +
+                    " WHERE c.Continent = ?" +
+                    " ORDER BY cty.Population DESC" +
+                    " LIMIT ?";
+
+            //Sets up the prepared statement
+            PreparedStatement ps = db.connect(true).prepareStatement(sql);
+
+            //Assigns user input to parameterIndex
+            ps.setString(1, userContinent);
+            ps.setInt(2, limit);
+
+            // Execute SQL statement
+            ResultSet rset = ps.executeQuery();
+
+            //Sets the filename for the CSV file and creates a path to
+            String fileName = "csv/cities/set_cities_in_continent/" + "Cities in " + userContinent + ".csv";
+
+            //Creates an empty ArrayList of cities for printing
+            ArrayList<City> cities = new ArrayList<>();
+
+            // Check one is returned
+            while (rset.next()) {
+                City cty = new City();
+                cty.cityName = rset.getString("Name");
+                CSVCreator.createCSV(fileName, rset);
+                cities.add(cty);
+            }
+            return cities;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get cities in selected continent");
+            return null;
+
+        }
+
+    }
+
 }
